38ab48efbcee3a17e282b3fed118945bb56c5eb9
Junk1d <66874533+Junk1d@users.noreply.github.com>
2024-08-16 10:03:53 +0200

    Update api

--- api.js
+++ api.js
@@ -1,48 +1,79 @@
-const api = axios.create({
-    baseURL: "http://localhost:3002",
-});
+import fs from "fs";
+import axios from "axios";
+import { Buffer } from "buffer";
 
+let OAuthSp;
+let serialize = function (obj) {
+  var str = [];
+  for (var p in obj) {
+    if (obj.hasOwnProperty(p)) {
+      str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+    }
+  }
+  return str.join("&");
+};
+let dataSp = JSON.parse(fs.readFileSync("./spotify.json", "utf8"));
+let api = axios.create({
+  baseURL: "https://api.spotify.com/v1",
+});
 api.interceptors.request.use(
-    (config) => {
-        const token = user.token;
-        if (token) {
-            config.headers.Authorization = token;
-        }
-        return config;
-    },
-    (error) => Promise.reject(error)
+  (config) => {
+    if (OAuthSp) {
+      config.headers.Authorization = "Bearer " + OAuthSp;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
 );
 
 api.interceptors.response.use(
-    (response) => response,
-    async (error) => {
-        const originalRequest = error.config;
-        if (!error.response) {
-            return Promise.reject(error);
-        }
-
-        if (error.response.status === 401 && !originalRequest._retry) {
-            originalRequest._retry = true;
-
-            try {
-                const refreshToken = localStorage.getItem("refresh_token");
-                const response = await axios.post(
-                    "http://localhost:3002/refreshToken",
-                    {},
-                    { headers: { refresh_token: refreshToken } }
-                );
-                const { token, refresh_token } = response.data;
-
-                setUser((current) => {
-                    return { ...current, token };
-                });
-                localStorage.setItem("refresh_token", refresh_token);
-
-                originalRequest.headers.Authorization = token;
-                return api(originalRequest);
-            } catch (error) {}
-        }
-
-        return Promise.reject(error);
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    if (!error.response) {
+      return Promise.reject(error);
     }
+
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      try {
+        const str =
+          (await process.env.CLIENT_ID_SPOTIFY) +
+          ":" +
+          (await process.env.CLIENT_SECRET_SPOTIFY);
+        const buff = await Buffer.from(str, "utf-8");
+        const base64 = await buff.toString("base64");
+        const responseRefesh = await axios.post(
+          "https://accounts.spotify.com/api/token",
+          await serialize({
+            refresh_token: dataSp.refresh_token,
+            grant_type: "refresh_token",
+          }),
+          {
+            headers: {
+              Authorization: "Basic " + base64,
+            },
+          }
+        );
+        fs.writeFileSync(
+          "./spotify.json",
+          JSON.stringify({
+            refresh_token: responseRefesh.data.refresh_token,
+          })
+        );
+        dataSp.refresh_token = responseRefesh.data.refresh_token;
+        OAuthSp = responseRefesh.data.access_token;
+        originalRequest.headers.Authorization =
+          "Bearer " + responseRefesh.data.access_token;
+        return api(originalRequest);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    return Promise.reject(error);
+  }
 );
+
+export default api;
--- main.js
+++ main.js
@@ -6,82 +6,83 @@ import http from "http";
 import "dotenv/config";
 import fs from "fs";
 import { Buffer } from "buffer";
+import api from "./api.js";
 
 var reqUrl;
 var state;
 var userID;
 var data;
-var username;
+// var username;
 var dataSp;
 var reqUrlSp;
 var expires_at;
-var OAuthSp;
-var OAuthTw;
-var OAuRefreshTokenSp;
+
+// var OAuthTw;
+// var OAuRefreshTokenSp;
 
 let radiobollerwagen =
-    "https://www.ffn.de/fileadmin/content/playlist-xml/radiobollerwagen.json";
+  "https://www.ffn.de/fileadmin/content/playlist-xml/radiobollerwagen.json";
 state = makeid(20);
-let serialize = function (obj) {
-    var str = [];
-    for (var p in obj) {
-        if (obj.hasOwnProperty(p)) {
-            str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
-        }
-    }
-    return str.join("&");
-};
+// let serialize = function (obj) {
+//   var str = [];
+//   for (var p in obj) {
+//     if (obj.hasOwnProperty(p)) {
+//       str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+//     }
+//   }
+//   return str.join("&");
+// };
 
-StartUpSpotify();
-async function StartUpSpotify() {
-    try {
-        dataSp = JSON.parse(fs.readFileSync("./spotify.json", "utf8"));
-        if (dataSp.refresh_token == "INITIAL_REFRESH_TOKEN") {
-            // TokenSpotify();
-        } else {
-            if (await refeshTokenSp()) {
-                Main();
-            }
-            // else TokenSpotify();
-        }
-    } catch (err) {
-        if (err.code == "ENOENT") {
-            console.error(err.code);
-            // TokenSpotify();
-        }
-    }
-}
-async function refeshTokenSp() {
-    const str =
-        (await process.env.CLIENT_ID_SPOTIFY) +
-        ":" +
-        (await process.env.CLIENT_SECRET_SPOTIFY);
-    const buff = await Buffer.from(str, "utf-8");
-    const base64 = await buff.toString("base64");
-    const responseRefesh = await axios
-        .post(
-            "https://accounts.spotify.com/api/token",
-            await serialize({
-                refresh_token: dataSp.refresh_token,
-                grant_type: "refresh_token",
-            }),
-            {
-                headers: {
-                    Authorization: "Basic " + base64,
-                },
-            }
-        )
-        .catch((err) => {
-            console.log(err);
-        });
+// StartUpSpotify();
+// async function StartUpSpotify() {
+//   try {
+//     dataSp = JSON.parse(fs.readFileSync("./spotify.json", "utf8"));
+//     if (dataSp.refresh_token == "INITIAL_REFRESH_TOKEN") {
+//       // TokenSpotify();
+//     } else {
+//       if (await refeshTokenSp()) {
+//         Main();
+//       }
+//       // else TokenSpotify();
+//     }
+//   } catch (err) {
+//     if (err.code == "ENOENT") {
+//       console.error(err.code);
+//       // TokenSpotify();
+//     }
+//   }
+// }
+// async function refeshTokenSp() {
+//   const str =
+//     (await process.env.CLIENT_ID_SPOTIFY) +
+//     ":" +
+//     (await process.env.CLIENT_SECRET_SPOTIFY);
+//   const buff = await Buffer.from(str, "utf-8");
+//   const base64 = await buff.toString("base64");
+//   const responseRefesh = await axios
+//     .post(
+//       "https://accounts.spotify.com/api/token",
+//       await serialize({
+//         refresh_token: dataSp.refresh_token,
+//         grant_type: "refresh_token",
+//       }),
+//       {
+//         headers: {
+//           Authorization: "Basic " + base64,
+//         },
+//       }
+//     )
+//     .catch((err) => {
+//       console.log(err);
+//     });
 
-    if (responseRefesh.status == 200) {
-        var d = new Date();
-        expires_at = d.getTime + responseRefesh.data.expires_in * 1000;
-        OAuthSp = responseRefesh.data.access_token;
-        return true;
-    } else return false;
-}
+//   if (responseRefesh.status == 200) {
+//     var d = new Date();
+//     expires_at = d.getTime + responseRefesh.data.expires_in * 1000;
+//     OAuthSp = responseRefesh.data.access_token;
+//     return true;
+//   } else return false;
+// }
 
 // TokenSpotify();
 
@@ -118,155 +119,141 @@ async function refeshTokenSp() {
 // }
 
 async function CheckExpired() {
-    var d = new Date();
-    var t = d.getTime;
-    t = t - 60000;
-    if (t >= expires_at) {
-        refeshTokenSp();
-        return true;
-    } else return false;
+  var d = new Date();
+  var t = d.getTime;
+  t = t - 60000;
+  if (t >= expires_at) {
+    refeshTokenSp();
+    return true;
+  } else return false;
 }
 
 async function SpCodeToToken(Code) {
-    const str =
-        (await process.env.CLIENT_ID_SPOTIFY) +
-        ":" +
-        (await process.env.CLIENT_SECRET_SPOTIFY);
-    const buff = await Buffer.from(str, "utf-8");
-    const base64 = await buff.toString("base64");
-    const responseTokenSp = await axios
-        .post(
-            "https://accounts.spotify.com/api/token",
-            serialize({
-                code: Code,
-                redirect_uri: "http://localhost:3000",
-                grant_type: "authorization_code",
-            }),
-            {
-                headers: {
-                    Authorization: "Basic " + base64,
-                },
-            }
-        )
-        .catch((err) => {
-            console.log(err);
-        });
+  const str =
+    (await process.env.CLIENT_ID_SPOTIFY) +
+    ":" +
+    (await process.env.CLIENT_SECRET_SPOTIFY);
+  const buff = await Buffer.from(str, "utf-8");
+  const base64 = await buff.toString("base64");
+  const responseTokenSp = await axios
+    .post(
+      "https://accounts.spotify.com/api/token",
+      serialize({
+        code: Code,
+        redirect_uri: "http://localhost:3000",
+        grant_type: "authorization_code",
+      }),
+      {
+        headers: {
+          Authorization: "Basic " + base64,
+        },
+      }
+    )
+    .catch((err) => {
+      console.log(err);
+    });
 
-    if ((await responseTokenSp.status) == 200) {
-        console.log(responseTokenSp.data);
-        try {
-            var d = new Date();
-            expires_at = d.getTime + responseTokenSp.data.expires_in * 1000;
-            OAuthSp = responseTokenSp.data.access_token;
-            OAuRefreshTokenSp = responseTokenSp.data.refresh_token;
+  if ((await responseTokenSp.status) == 200) {
+    console.log(responseTokenSp.data);
+    try {
+      var d = new Date();
+      expires_at = d.getTime + responseTokenSp.data.expires_in * 1000;
+      OAuthSp = responseTokenSp.data.access_token;
+      OAuRefreshTokenSp = responseTokenSp.data.refresh_token;
 
-            fs.writeFileSync(
-                "./spotify.json",
-                JSON.stringify({
-                    refresh_token: responseTokenSp.data.refresh_token,
-                })
-            );
-        } catch (error) {
-            console.error(error);
-            console.log("hist");
-            // TokenSpotify();
-            return;
-        }
-
-        Main();
+      fs.writeFileSync(
+        "./spotify.json",
+        JSON.stringify({
+          refresh_token: responseTokenSp.data.refresh_token,
+        })
+      );
+    } catch (error) {
+      console.error(error);
+      console.log("hist");
+      // TokenSpotify();
+      return;
     }
-    // else TokenSpotify();
+
+    Main();
+  }
+  // else TokenSpotify();
 }
-
+Main();
 function Main() {
-    let lastAdded;
-    async function runnabale() {
-        fetch(radiobollerwagen, { method: "GET" })
-            .then((res) => res.json())
-            .then((json) => {
-                if (json.songs[0].title != lastAdded) {
-                    console.log(json.songs[0].title);
-                    lastAdded = json.songs[0].title;
-                    addToPlaylist(json.songs[0].title, json.songs[0].artist);
-                }
-            });
-        try {
-            const response = await axios.get(
-                "https://api.spotify.com/v1/playlists/3WeU50f5AqaM0Z0aYQNoEz",
-                {
-                    headers: { Authorization: "Bearer " + OAuthSp },
-                }
-            );
-            console.log(response.data.tracks.total);
-            let d = new Date();
-            let t = d.getTime();
-            t = t - 2 * 60 * 60 * 1000;
-            let dateSong = new Date(response.data.tracks.items[0].added_at);
-            let timeSong = dateSong.getTime();
-            if (t >= timeSong) {
-                const responsee = await axios.delete(
-                    "https://api.spotify.com/v1/playlists/3WeU50f5AqaM0Z0aYQNoEz/tracks",
-                    {
-                        headers: {
-                            Authorization: "Bearer " + OAuthSp,
-                        },
-                        data: {
-                            tracks: [
-                                {
-                                    uri: response.data.tracks.items[0].track
-                                        .uri,
-                                    positions: [0],
-                                },
-                            ],
-                        },
-                    }
-                );
-            }
-        } catch (error) {
-            console.log(error);
-            return;
+  let lastAdded;
+  async function runnabale() {
+    fetch(radiobollerwagen, { method: "GET" })
+      .then((res) => res.json())
+      .then((json) => {
+        if (json.songs[0].title != lastAdded) {
+          console.log(json.songs[0].title);
+          lastAdded = json.songs[0].title;
+          addToPlaylist(json.songs[0].title, json.songs[0].artist);
         }
+      });
+    try {
+      const response = await api.get("/playlists/3WeU50f5AqaM0Z0aYQNoEz");
+      console.log(response.data.tracks.total);
+      let d = new Date();
+      let t = d.getTime();
+      t = t - 2 * 60 * 60 * 1000;
+      let dateSong = new Date(response.data.tracks.items[0].added_at);
+      let timeSong = dateSong.getTime();
+      if (t >= timeSong) {
+        const responsee = await api.delete(
+          "/playlists/3WeU50f5AqaM0Z0aYQNoEz/tracks",
+          {
+            data: {
+              tracks: [
+                {
+                  uri: response.data.tracks.items[0].track.uri,
+                  positions: [0],
+                },
+              ],
+            },
+          }
+        );
+      }
+    } catch (error) {
+      console.log(error.code);
+      return;
     }
-    runnabale();
-    var timerID = setInterval(runnabale, 60 * 1000);
+  }
+  runnabale();
+  var timerID = setInterval(runnabale, 60 * 1000);
 }
 
 async function addToPlaylist(title, artist) {
-    let findUri;
+  let findUri;
+  try {
+    let res = await api.get(
+      `/search?q=${title.replace(" ", "+")}+${artist.replace(
+        " ",
+        "+"
+      )}&type=track&limit=1`
+    );
+    findUri = res.data.tracks.items[0].uri;
     try {
-        let res = await axios.get(
-            `https://api.spotify.com/v1/search?q=${title.replace(
-                " ",
-                "+"
-            )}+${artist.replace(" ", "+")}&type=track&limit=1`,
-            { headers: { Authorization: "Bearer " + OAuthSp } }
-        );
-        findUri = res.data.tracks.items[0].uri;
-        try {
-            let res = await axios.post(
-                `https://api.spotify.com/v1/playlists/3WeU50f5AqaM0Z0aYQNoEz/tracks`,
-                { uris: [findUri] },
-                { headers: { Authorization: "Bearer " + OAuthSp } }
-            );
-        } catch (error) {
-            console.log(error);
-            return;
-        }
+      let res = await api.post(`/playlists/3WeU50f5AqaM0Z0aYQNoEz/tracks`, {
+        uris: [findUri],
+      });
     } catch (error) {
-        console.log(error);
-        return;
+      console.log(error.code);
+      return;
     }
+  } catch (error) {
+    console.log(error.code);
+    return;
+  }
 }
 
 function makeid(length) {
-    var result = "";
-    var characters =
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    var charactersLength = characters.length;
-    for (var i = 0; i < length; i++) {
-        result += characters.charAt(
-            Math.floor(Math.random() * charactersLength)
-        );
-    }
-    return result;
+  var result = "";
+  var characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
 }
